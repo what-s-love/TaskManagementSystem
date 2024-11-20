e5559142ca1dc6d3d6927351ff8e70cd0abdda6b
VladislavK <barabanabanan@gmail.com>
2024-11-20 19:48:34 +0600

    Added some dependencies to pom; Created environment file .env

--- src/main/java/kg/tasksystem/model/Comment.java
+++ src/main/java/kg/tasksystem/model/Comment.java
@@ -2,16 +2,24 @@ package kg.tasksystem.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+@Getter
+@Setter
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "COMMENTS")
 public class Comment {
     @Id
     @Column(name = "ID", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "CONTENT")
+    @Column(name = "CONTENT", columnDefinition = "CHARACTER LARGE OBJECT(0, 0) not null")
+    @Lob
     private String content;
     @OneToOne
     @JoinColumn(name = "AUTHORID", nullable = false)
@@ -21,5 +29,4 @@ public class Comment {
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "TASKID", nullable = false)
     private Task task;
-
 }
--- src/main/java/kg/tasksystem/model/Task.java
+++ src/main/java/kg/tasksystem/model/Task.java
@@ -2,28 +2,41 @@ package kg.tasksystem.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "TASKS")
 public class Task {
     @Id
     @Column(name = "ID", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "TITLE", nullable = false, length = 50)
     private String title;
     @Column(name = "DESCRIPTION", columnDefinition = "CHARACTER LARGE OBJECT(0, 0) not null")
     @Lob
     private String description;
+    @NotNull
+    @Column(name = "STATUS", nullable = false)
     private String status;
+    @NotNull
+    @Column(name = "PRIORITY", nullable = false)
     private String priority;
     @NotNull
-    @Column(name = "TIME", nullable = false)
+    @Column(name = "CREATETIME", nullable = false)
     private LocalDateTime createdAt;
-    @NotNull
-    @Column(name = "TIME", nullable = false)
+    @Column(name = "UPDATETIME", nullable = false)
     private LocalDateTime updatedAt;
     @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Comment> comments;
