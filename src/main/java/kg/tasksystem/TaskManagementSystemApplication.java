88c7f4aaf14ccbabd4b65e23aa29f7626acf5480
what-s-love <barabanabanan@gmail.com>
2024-11-27 03:35:34 +0600

    Some troubles with git, reset changes to Compose branch

--- src/main/java/kg/tasksystem/TaskManagementSystemApplication.java
+++ src/main/java/kg/tasksystem/TaskManagementSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskManagementSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementSystemApplication.class, args);
 	}
--- src/main/java/kg/tasksystem/dto/TaskDto.java
+++ src/main/java/kg/tasksystem/dto/TaskDto.java
@@ -2,8 +2,6 @@ package kg.tasksystem.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
